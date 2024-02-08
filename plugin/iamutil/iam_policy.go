16a4af3ff4bd171a2063709f0a8d0a38362f256b
James Cottrill <james@slatedata.co.uk>
2024-02-08 10:36:00 +0000

    feat: Add support for adding bindings with conditions

--- plugin/iamutil/iam_policy.go
+++ plugin/iamutil/iam_policy.go
@@ -32,8 +32,9 @@ type Condition struct {
 }
 
 type PolicyDelta struct {
-	Roles util.StringSet
-	Email string
+	Roles     util.StringSet
+	Email     string
+	Condition *Condition
 }
 
 func (p *Policy) AddBindings(toAdd *PolicyDelta) (changed bool, updated *Policy) {
@@ -45,10 +46,11 @@ func (p *Policy) RemoveBindings(toRemove *PolicyDelta) (changed bool, updated *P
 }
 
 func (p *Policy) ChangeBindings(toAdd *PolicyDelta, toRemove *PolicyDelta) (changed bool, updated *Policy) {
+	//If No toAdd and no toRemove, return False
 	if toAdd == nil && toRemove == nil {
 		return false, p
 	}
-
+	//Convert email into serviceAccountEmail
 	var toAddMem, toRemoveMem string
 	if toAdd != nil {
 		toAddMem = fmt.Sprintf(ServiceAccountMemberTmpl, toAdd.Email)
@@ -60,28 +62,37 @@ func (p *Policy) ChangeBindings(toAdd *PolicyDelta, toRemove *PolicyDelta) (chan
 	changed = false
 
 	newBindings := make([]*Binding, 0, len(p.Bindings))
-	alreadyAdded := make(util.StringSet)
-
+	alreadyAdded := make([]*Binding, 0)
+	//Loop through the existing policyBindings
 	for _, bind := range p.Bindings {
+		//for each binding, set of existing members
 		memberSet := util.ToSet(bind.Members)
-
+		//If adding
 		if toAdd != nil {
-			if toAdd.Roles.Includes(bind.Role) {
+			//If the binding role is in the list of roles and they have the same condition.
+			if toAdd.Roles.Includes(bind.Role) && toAdd.Condition == bind.Condition {
 				changed = true
-				alreadyAdded.Add(bind.Role)
+				// Add the binding to the set of alreadyAdded bindings
+				alreadyAdded = append(alreadyAdded, bind)
+				// Add the member to the memberSet
 				memberSet.Add(toAddMem)
 			}
 		}
 
 		if toRemove != nil {
-			if toRemove.Roles.Includes(bind.Role) {
+			// If the list of roles includes one of the roles to remove, and they have the same condition
+			// TODO:: Do we want to remove users regardless of the condition?
+			if toRemove.Roles.Includes(bind.Role) && toAdd.Condition == bind.Condition {
 				if memberSet.Includes(toRemoveMem) {
 					changed = true
+					//remove the member form the memberSet
 					delete(memberSet, toRemoveMem)
 				}
 			}
 		}
 
+		//If there is more than one member in the memberSet, add the binding to the memberSet using the existing
+		//binding Condition
 		if len(memberSet) > 0 {
 			newBindings = append(newBindings, &Binding{
 				Role:      bind.Role,
@@ -90,15 +101,20 @@ func (p *Policy) ChangeBindings(toAdd *PolicyDelta, toRemove *PolicyDelta) (chan
 			})
 		}
 	}
+	// End loop through existing bindings
 
 	if toAdd != nil {
+		//For each role being added
 		for r := range toAdd.Roles {
-			if !alreadyAdded.Includes(r) {
+			binding := &Binding{
+				Role:      r,
+				Members:   []string{toAddMem},
+				Condition: toAdd.Condition,
+			}
+			// If the binding (role and condition) didn't already exist, add it
+			if !BindingExists(alreadyAdded, binding) {
 				changed = true
-				newBindings = append(newBindings, &Binding{
-					Role:    r,
-					Members: []string{toAddMem},
-				})
+				newBindings = append(newBindings, binding)
 			}
 		}
 	}
@@ -111,4 +127,15 @@ func (p *Policy) ChangeBindings(toAdd *PolicyDelta, toRemove *PolicyDelta) (chan
 		}
 	}
 	return false, p
+}
+
+func BindingExists(existingBindings []*Binding, binding *Binding) (exists bool) {
+	// For each binding that already existed, loop through and see if the same role and condition
+	// already existed. If it does, return true, else return false
+	for _, bind := range existingBindings {
+		if bind.Role == binding.Role && bind.Condition == binding.Condition {
+			return true
+		}
+	}
+	return false
 }
